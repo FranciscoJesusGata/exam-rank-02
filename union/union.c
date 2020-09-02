0b76efb2f839b9740bee7832695edfb52345c9ca
Francisco Jesús Gata <fgata-va@student.42madrid.com>
2020-09-02 12:52:09 +0200

    Inter and union completed

--- union/union.c
+++ union/union.c
@@ -1,51 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inter.c                                            :+:      :+:    :+:   */
+/*   union.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgata-va <fgata-va@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/02 10:23:34 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/09/02 10:23:48 by fgata-va         ###   ########.fr       */
+/*   Created: 2020/09/02 12:03:34 by fgata-va          #+#    #+#             */
+/*   Updated: 2020/09/02 12:51:11 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int ok[255];
+	int ok[128];
 	int i;
 	int j;
+	int k;
 
-	if (argc == 3 && argv[1] && argv[2])
+	if (argc == 3)
 	{
 		i = 0;
-		while(ok[i])
+		while (ok[i])
 		{
 			ok[i] = 0;
 			i++;
 		}
-		i = 2;
+		i = 1;
 		j = 0;
-		while(i > 0)
+		k = 1;
+		while (k < 3)
 		{
-			if (!argv[i][j])
+			if (i >= 3)
 			{
-				i--;
+				i = 1;
+				j = -1;
+				k++;
+			}
+			else if (!argv[i][j])
+			{
+				i++;
 				j = -1;
 			}
-			else if (i == 2 && argv[i][j])
+			else if (k == 1 && argv[i][j])
 				ok[(int)argv[i][j]] = 1;
-			if (i == 1 && argv[i][j] && ok[(int)argv[i][j]] == 1)
+			else if (k == 2 && argv[i][j] && ok[(int)argv[i][j]] == 1)
 			{
-				write(1, &argv[i][j], 1);
 				ok[(int)argv[i][j]] = 0;
+				write(1, &argv[i][j], 1);
 			}
 			j++;
 		}
+
 	}
-	write(1, "\n", 1);
-	return (0);
+	write(1,"\n",1);
+	return(0);
 }
