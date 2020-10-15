f2ce6d3b596ca426bb953b982249fd501852a899
Francisco jesus Gata valdes <fgata-va@c1r4s6.42madrid.com>
2020-10-15 13:35:33 +0200

    Flags are in process

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 19:23:51 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/10/13 20:40:28 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/10/15 13:35:08 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_strlen(char *s)
 	return i;
 }
 
-void	ft_putnbr(int n, char *base, int *printed)
+void	ft_putnbr(long int n, char *base, int *printed)
 {
 	int	len = ft_strlen(base);
 	int	c = 0;
@@ -29,7 +29,7 @@ void	ft_putnbr(int n, char *base, int *printed)
 	*printed += write(1, (base + c), 1);
 }
 
-void	ft_nbrlen(int n, int base_len)
+int	ft_nbrlen(long int n, int base_len)
 {
 	int	i = 1;
 
@@ -47,7 +47,9 @@ int		ft_printf(const char *f, ...)
 	int	i = 0;
 	int	printed = 0;
 	int	len = 0;
-	int	n = 0;
+	char *hex = "0123456789abcdef";
+	char *dec = "0123456789";
+	long int	n = 0;
 	unsigned int	h = 0;
 	char *str = NULL;
 	int	w = 0;
@@ -61,6 +63,9 @@ int		ft_printf(const char *f, ...)
 			printed += write(1, (f + i), 1);
 		else
 		{
+			p = 0;
+			p_l = 0;
+			w = 0;
 			while(f[i])
 			{
 				i++;
@@ -73,6 +78,16 @@ int		ft_printf(const char *f, ...)
 						len = ft_strlen(str);
 					if (p == 1 && p_l < len)
 						len -= p_l;
+				}
+				else if (f[i] == 'd')
+				{
+					n = va_arg(ap, long int);
+					len = ft_nbrlen(n, 10);
+				}
+				else if (f[i] == 'x')
+				{
+					h = va_arg(ap, unsigned int);
+					len = ft_nbrlen(n, 16);
 				}
 				while (w > len)
 				{
@@ -88,15 +103,19 @@ int		ft_printf(const char *f, ...)
 				}
 				else
 				{
-					if (f[i] == 'd')
-						
+					while (p == 1 && p_l > len)
+					{
+						write(1, "0", 1);
+						p_l--;
+					}
+					if (f[i] == 'd' && (n > 0 && p == 1 && p_l > 0))
+						ft_putnbr(n, dec, &printed);
 					else if (f[i] == 'x')
-						
+						ft_putnbr(h, hex, &printed);
 				}
-				p = 0;
-				p_l = 0;
 			}
 		}
 		i++;
 	}
+	return (printed);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: fgata-va <fgata-va@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 20:01:37 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/10/13 20:03:01 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/10/15 10:30:44 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <unistd.h>
+# include <stdarg.h>
 
 	int ft_printf(const char *f, ... );
 
