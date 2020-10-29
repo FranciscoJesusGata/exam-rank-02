c52efac59f482e481c1aba7e6215f2dbff23f32f
Francisco jesus Gata valdes <fgata-va@c1r3s1.42madrid.com>
2020-10-29 12:42:27 +0100

    Stupid error

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 18:49:32 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/10/29 09:53:16 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/10/29 12:41:59 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int		main(void)
 		free(line);
 		line = NULL;
 	}
-	printf("%s\n", line);
+	printf("%s", line);
 	free(line);
 	line = NULL;
 	return (0);
